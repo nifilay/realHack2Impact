35f6eb4248c02aeca9cd470989d2beff090e33f1
Aidan Shahmaram <aidanashahmaram@gmail.com>
2025-05-17 16:48:11 -0700

    set up base for donation update api

--- server/controllers/qr_controller.js
+++ server/controllers/qr_controller.js
@@ -2,10 +2,19 @@ const qr_schema = require('../models/QR');
 
 
 const qr_create = async(req, res) => {
-    const {donation_id, email} = req.body;
+    const {donation_id} = req.body;
 
-    const payload = JSON.stringify({donation_id, email});
+    const payload = JSON.stringify({donation_id});
 
     const qr_data_url = await QRCode.toDataURL(payload);
     res.json({ qrCode: qr_data_url });
+}
+
+
+const qr_scan = async(req, res) => {
+    const {donation_id, location} = req.body;
+
+    const donation = qr_schema.findOne({donation_id: donation_id});
+
+
 }
