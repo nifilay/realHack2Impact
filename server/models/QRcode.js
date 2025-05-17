caec6fba8dc9283f9efc5d198b1536eb228b3412
Aidan Shahmaram <aidanashahmaram@gmail.com>
2025-05-17 02:25:25 -0700

    location no longer required upon qr code creation

--- server/models/QRcode.js
+++ server/models/QRcode.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const qrSchema = new mongoose.Schema({
   email:    { type: String, required: true, unique: true },
-  locations: [{ type: String, required: true }],
+  locations: [{ type: String}],
   scannedAt: { type: Date, default: Date.now },
   driver: {type: String}
 });
