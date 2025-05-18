12c44b85419ffbd31d272f0dff831d9df3ca271e
Aidan Shahmaram <aidanashahmaram@gmail.com>
2025-05-17 21:00:59 -0700

    changed employee model to include name

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose';
+\import mongoose from 'mongoose';
 
 const employeeSchema = new mongoose.Schema({
   location: { type: String},
-  driver: {type: String}
+  name: {type: String}
 });
 
 export default mongoose.model('Employee', employeeSchema);
