330c831bc1fc8e158fc0a2d45ca66eee3f755a12
Aidan Shahmaram <aidanashahmaram@gmail.com>
2025-05-17 18:36:00 -0700

    updated Donation model to hold clothing type and qr url

--- server/models/Donation.js
+++ server/models/Donation.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
 const donationSchema = new mongoose.Schema({
-  email:    { type: String, required: true, unique: true },
-  locations: [{ type: String}],
-  createdAt: { type: Date, default: Date.now },
-  driver: {type: String}
+    email:    { type: String, required: true, unique: true },
+    locations: [{ type: String}],
+    createdAt: { type: Date, default: Date.now },
+    driver: {type: String},
+    clothing_type: {type: String},
+    qr_code_url: {type: String}
 });
 
 export default mongoose.model('Donation', donationSchema);
